6124da2f9d5c648bd2fe2ab04186ee98918d1bbb
cris-al <albornozcristian94@gmail.com>
2022-09-15 14:18:55 -0300

    cloudinary

--- api/src/router/routers.js
+++ api/src/router/routers.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const {register, login, getUsers , logout } = require('../controllers/users-controller')
+const {register, login, getUsers , logout, upDateUser } = require('../controllers/users-controller')
 const {createEvent, getEvents} = require('../controllers/events-controller')
-
+const {fileUpload} = require('../helpers/fileUpload')
 
 
 router.post('/login', login )
 router.post('/register' , register)
 router.get('/logout', logout)
+router.put('/user/:id/profile', fileUpload, upDateUser)
 router.post('/createEvent',  createEvent)
 router.get('/users' , passport.authenticate('jwt-auth', { session: false }),getUsers)
 router.post('/createEvent', createEvent)
