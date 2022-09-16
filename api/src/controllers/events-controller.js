eec3629f96120bd81768e521731ded2d22381d2e
cris-al <albornozcristian94@gmail.com>
2022-09-16 10:52:33 -0300

    cludinary for events

--- api/src/controllers/events-controller.js
+++ api/src/controllers/events-controller.js
@@ -1,10 +1,17 @@
 const { EventsCreated, Event} = require('../db')
 const fs = require('fs')
+const uploadImage = require('../helpers/cloudinary')
+const fsExtra = require('fs-extra')
 
 const createEvent = async (req, res) => {
     const {description, price, date, artist, place, stock, category} = req.body
-
+    var result;
     try {
+        if(req.files?.image){
+            result = await uploadImage(req.files.image.tempFilePath);
+            await fsExtra.unlink(req.files.image.tempFilePath)
+        }
+
         const newEvent = await EventsCreated.create({
             description,
             price,
@@ -12,7 +19,8 @@ const createEvent = async (req, res) => {
             artist,
             place,
             stock,
-            category
+            category,
+            image: result?result.secure_url:'undefined'
         })
         res.json({
             eventsCreated: newEvent
