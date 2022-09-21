b3a29a21722a1b2aef1c74fbec5d283abcf5d599
Luis Daniel Rios <luisdanielrios@comuna18.com>
2022-09-21 12:19:56 -0500

    deploy?

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,28 @@
-import React , {Suspense }from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import Loading from "./components/UI/Loading";
-import {Provider} from 'react-redux';
-import {store} from '../src/store/index'
+import { Provider } from "react-redux";
+import { store } from "../src/store/index";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
+axios.defaults.baseURL = process.env.VITE_APP_API || "http://localhost:3001";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <Suspense fallback={<><Loading/></>}>
+    <Suspense
+      fallback={
+        <>
+          <Loading />
+        </>
+      }
+    >
       <Provider store={store}>
-          <App />
-           </Provider>
-      </Suspense>
+        <App />
+      </Provider>
+    </Suspense>
   </React.StrictMode>
 );
