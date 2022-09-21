1850d4065e3b73effa5ebed4bf0ac6b572157f36
Luis Daniel Rios <luisdanielrios@comuna18.com>
2022-09-21 13:24:36 -0500

    another deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,6 @@ import Loading from "./components/UI/Loading";
 import { Provider } from "react-redux";
 import { store } from "../src/store/index";
 import axios from "axios";
-import dotenv from "dotenv";
 
 axios.defaults.baseURL =
   import.meta.env.VITE_APP_API || "http://localhost:3001";
