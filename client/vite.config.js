3e7238333d5dbcbfc8d1b6346fcf4e318b560ce9
Luis Daniel Rios <luisdanielrios@comuna18.com>
2022-09-21 14:22:37 -0500

    another deploy again again

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import EnvironmentPlugin from "vite-plugin-environment";
+// import EnvironmentPlugin from "vite-plugin-environment";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    EnvironmentPlugin(
-      { API: "https://pfhenryg.herokuapp.com" },
-      { defineOn: "import.meta.env" }
-    ),
-  ],
+  plugins: [react()],
 });
+
+// EnvironmentPlugin(
+//   { API: "https://pfhenryg.herokuapp.com" },
+//   { defineOn: "import.meta.env" }
+// ),
