0352813e8f5f580b02bb9191c2ff34e9df8de01b
Jonatan Musciachio <100133245+jonafm14@users.noreply.github.com>
2022-09-26 09:38:17 -0300

    feedback mercadopago

--- api/src/controllers/payments-controller.js
+++ api/src/controllers/payments-controller.js
@@ -6,7 +6,7 @@ const mercadopagoPayment = async (req, res) => {
         let preference = {
             items: [
                 {
-                    "title": "Realizar la compra",
+                    "title": "Finalizar la compra",
                     "description": "Con esta compra eres capaz de unirte a los eventos",
                     "category_id": "categoria123",
                     "quantity": 1,
@@ -14,17 +14,17 @@ const mercadopagoPayment = async (req, res) => {
                 }
             ],
             back_urls: {
-                failure: "/failure",
+                failure: "http://127.0.0.1:5173/payment/failure",
                 pending: "/pending",
-                success: "http://127.0.0.1:5173",
+                success: "http://127.0.0.1:5173/payment/success",
             },
             auto_return: "approved",
         };
 
         mercadopago.preferences.create(preference)
             .then(function (response) {
-                console.log(response)
-                res.json({ global: response.body.init_point });
+                console.log(response.body)
+                res.json(response.body.init_point );
             })
             .catch(function (error) {
                 console.log(error);
@@ -35,7 +35,6 @@ const mercadopagoPayment = async (req, res) => {
 }
 
 
-
 module.exports = {
-    mercadopagoPayment
+    mercadopagoPayment,
 }
