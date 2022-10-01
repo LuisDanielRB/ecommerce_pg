7c0400961e1719fc20870e61cf88ec70d375539e
lucho9595 <lucianocoronel48@gmail.com>
2022-10-01 12:54:32 -0300

    malditocarousel

--- client/src/components/UI/Carousel.jsx
+++ client/src/components/UI/Carousel.jsx
@@ -1,3 +1,4 @@
+import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from '@heroicons/react/24/outline';
 import React, { useState } from 'react';
 import { useRef } from 'react';
 import Img1 from "../../assets/carousel/Img1.jpg"
@@ -9,21 +10,30 @@ export default function Carousel() {
   const mov = useRef(null);
 
   const goToNext = () => {
-    //comprobamos que el mov tenga elementos
+    // comprobamos que el mov tenga elementos
     if (mov.current.children.length > 0) {
-      console.log(goToNext)
-      //obtenemos el primer elemento del mov
+
+      //   //obtenemos el primer elemento del mov
       const primerElemento = mov.current.children[0];
-      console.log(primerElemento)
+
+      mov.current.style.transition = `5000ms ease-out all`;
+
       const tamañoSlide = mov.current.children[0].offsetWidth
-      mov.current.style.transition = `300ms ease-out all`;
       mov.current.style.transform = `translateX(-${tamañoSlide}px)`
+
+      const transicion = () => {
+        mov.current.style.transition = 'none';
+        mov.current.style.transform = `translateX(0)`;
+
+      }
+
+      mov.current.addEventListener('transtitionend', transicion)
     }
   }
 
-    const goToPrev = () => {
-      console.log(mov.current)
-    };
+  const goToPrev = () => {
+    console.log(goToPrev)
+  };
 
 
 
@@ -31,19 +41,19 @@ export default function Carousel() {
     <section>
       <div className='relative'>
         <ul id='slider' ref={mov}>
-          <li className='h-[50vh] relative'>
+          <li className='h-[50vh] relative transition-all-duration-300'>
             <img className='h-full w-full' src={Img1} alt="" />
             <div className='absolute  top-0 left-0 h-full w-full flex'>
               <h2 className='text-4xl font-bold text-white my-auto w-full text-center px-20'>Consigue los mejores boletos, a los mejores precios!</h2>
             </div>
           </li>
-          <li className='h-[50vh] relative hidden'>
+          <li className='h-[50vh] relative hidden transition-all-duration-300'>
             <img className='h-full w-full' src={Img2} alt="" />
             <div className='absolute  top-0 left-0 h-full w-full flex'>
               <h2 className='text-4xl font-bold text-white my-auto w-full text-center px-20'>Consigue los mejores boletos, a los mejores precios!</h2>
             </div>
           </li>
-          <li className='h-[50vh] relative hidden'>
+          <li className='h-[50vh] relative hidden transition-all-duration-300'>
             <img className='h-full w-full' src={Img3} alt="" />
             <div className='absolute  top-0 left-0 h-full w-full flex'>
               <h2 className='text-4xl font-bold text-white my-auto w-full text-center px-20'>Consigue los mejores boletos, a los mejores precios!</h2>
@@ -51,14 +61,12 @@ export default function Carousel() {
           </li>
         </ul>
         <div className='absolute px-5 flex h-full w-full top-0 left-0 '>
-          <div className='my-auto  w-full flex justify-between'>
-            <button onClick={goToPrev} className='bg-white p-2 rounded-full bg-opacity-80 shadow-lg'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-6 h-6">
-              </svg>
+          <div className='my-auto  w-full flex justify-between '>
+            <button onClick={goToPrev} >
+              <ArrowLeftCircleIcon viewBox="0 0 30 30" className="w-20 h-20" />
             </button>
-            <button onClick={goToNext} className='bg-white p-2 rounded-full bg-opacity-80 shadow-lg '>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  className="w-6 h-6">
-              </svg>
+            <button onClick={goToNext} >
+              <ArrowRightCircleIcon viewBox="0 0 30 30" className="w-20 h-20" />
             </button>
           </div>
         </div>
