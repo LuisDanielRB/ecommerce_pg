972d9f0c0b2a3297dbf579ebf7b43e47f76e7eaa
Luciano Coronel <86379664+lucho9595@users.noreply.github.com>
2022-10-04 11:58:45 -0300

    editprofile

--- client/src/components/UI/Navbar.jsx
+++ client/src/components/UI/Navbar.jsx
@@ -33,8 +33,10 @@ function Navbar() {
     e.preventDefault();
     dispatch(userSignOut("user"));
     logOut();
-    navigate('/login')
+    navigate('/')
   };
+
+  const id = user ? user.id : false;
 
   const navigation = [
     { name: "Dashboard", href: "#", current: true },
@@ -45,8 +47,8 @@ function Navbar() {
 
   const userNavigation = [
     { name: "Your Profile", href: "#" },
-    { name: "Settings", href: `/private/user/profile` },
-    { name: "Log out", href: "#" },
+    { name: "Settings", href: `/private/user/${id}/profile`, current: true },
+    { name: "Log out", href: "#", current: true },
     { name: "Dashboard", href: "/private/admindashboard" },
   ];
 
@@ -162,30 +164,29 @@ function Navbar() {
                         <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                           <Menu.Item>
                             {({ active }) => (
-                              <button
+                              <a
                                 onClick={handleClick}
-                                href="#"
                                 className={classNames(
                                   active ? "bg-gray-100" : "",
                                   "block py-2 px-4 text-sm text-gray-700"
                                 )}
                               >
                                 Logout
-                              </button>
+                              </a>
                             )}
                           </Menu.Item>
                           <Menu.Item>
                             {({ active }) => (
-                              <button
-                                onClick={handleClick}
-                                href={`/private/user/${user.id}/profile`}
+                              <a
+                                onClick={() => null}
+                                href={`/private/user/${id}/profile`}
                                 className={classNames(
                                   active ? "bg-gray-100" : "",
                                   "block py-2 px-4 text-sm text-gray-700"
                                 )}
                               >
                                 Settings
-                              </button>
+                              </a>
                             )}
                           </Menu.Item>
                           <Menu.Item>
