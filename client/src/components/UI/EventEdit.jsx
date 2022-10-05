17dac1b2a792e027d60984952718b277017683e6
Luis Daniel Rios <luisdanielrios@comuna18.com>
2022-10-05 16:06:49 -0500

    fixes dashboard v1.4

--- client/src/components/UI/EventEdit.jsx
+++ client/src/components/UI/EventEdit.jsx
@@ -20,6 +20,26 @@ function EventEdit({ id }) {
   } = useSelector((state) => state.eventsDetail);
   const [input, setInput] = useState({});
 
+  async function handleFile(e) {
+    e.preventDefault();
+    let image = e.target.files[0];
+    let data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "pkokipva");
+    fetch("https://api.cloudinary.com/v1_1/dzjonhhps/image/upload", {
+      method: "POST",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setInput({
+          ...input,
+          image: res.secure_url,
+          imageId: res.public_id,
+        });
+      });
+  }
+
   function handleInputChange(e) {
     setInput({
       ...input,
@@ -44,7 +64,7 @@ function EventEdit({ id }) {
       <div className="pt-5">
         <div className="flex justify-end">
           <a
-            href="/private/admindashboard"
+            href="/admindashboard"
             type="button"
             className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
@@ -153,28 +173,7 @@ function EventEdit({ id }) {
               </div>
             </div>
 
-            <div className="sm:col-span-3">
-              <label
-                htmlFor="country"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Country
-              </label>
-              <div className="mt-1">
-                <select
-                  id="country"
-                  name="country"
-                  autoComplete="country-name"
-                  className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                >
-                  <option>United States</option>
-                  <option>Canada</option>
-                  <option>Mexico</option>
-                </select>
-              </div>
-            </div>
-
-            {/* <div className="sm:col-span-6">
+            <div className="sm:col-span-6">
               <label
                 htmlFor="photo"
                 className="block text-sm font-medium text-gray-700"
@@ -192,78 +191,38 @@ function EventEdit({ id }) {
                     <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
                   </svg>
                 </span>
-                <button
-                  type="button"
-                  className="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Change
-                </button>
+                <span className="ml-4 flex flex-shrink-0 items-center space-x-4">
+                  <input
+                    onChange={handleFile}
+                    type="file"
+                    accept="image/*"
+                    className="rounded-md bg-white font-medium text-purple-600 hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2"
+                  />
+                  <span className="ml-4 flex-shrink-0"></span>
+                </span>
               </div>
             </div>
-
-            <div className="sm:col-span-6">
-              <label
-                htmlFor="cover-photo"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Cover photo
-              </label>
-              <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                <div className="space-y-1 text-center">
-                  <svg
-                    className="mx-auto h-12 w-12 text-gray-400"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 48 48"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                  <div className="flex text-sm text-gray-600">
-                    <label
-                      htmlFor="file-upload"
-                      className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
-                    >
-                      <span>Upload a file</span>
-                      <input
-                        id="file-upload"
-                        name="file-upload"
-                        type="file"
-                        className="sr-only"
-                      />
-                    </label>
-                    <p className="pl-1">or drag and drop</p>
-                  </div>
-                  <p className="text-xs text-gray-500">
-                    PNG, JPG, GIF up to 10MB
-                  </p>
-                </div>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
 
       <div className="pt-5">
         <div className="flex justify-end">
-          <button
+          <a
+            href="/admindashboard"
             type="button"
             className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
             Cancel
-          </button>
-          <button
+          </a>
+          <a
+            href="/admindashboard"
             onClick={(e) => handleSubmit(e)}
             type="submit"
             className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
             Save
-          </button>
+          </a>
         </div>
       </div>
     </form>
